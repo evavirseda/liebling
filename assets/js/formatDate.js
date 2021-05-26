39f8d1d82d92fff3ab389c70fb94dcb0093e0d89
Diego <zitvura@gmail.com>
2021-05-26 11:59:31 +0200

    Translate recent posts dates

--- assets/js/formatDate.js
+++ assets/js/formatDate.js
@@ -1,29 +1,39 @@
-const LANGCODE = window.location.pathname.split('/')[1]
-let currentPostDate = document.getElementById("post-date")
-let relatedPostsDates = document.getElementsByClassName("related-post-date")
-let translatedPostDate = ''
+if (document.readyState === 'complete') {
+    const LANGCODE = window.location.pathname.split('/')[1]
+    let currentPostDate = document.getElementById('post-date')
+    let relatedPostsDates = document.getElementsByClassName('related-post-date')
+    let recentPostsDates = document.getElementsByClassName('m-recent-article__date')
+    let translatedPostDate = ''
 
-function translateDate(element, langcode){
-    switch(langcode){
-        case 'cn':
-            moment.locale('zh-cn')
-            translatedPostDate= moment(element.innerHTML).format('LL')
-            break;
-        case 'kr':
-            moment.locale('ko')
-            translatedPostDate= moment(element.innerHTML).format('LL')
-            break;
-        case 'jp':
-            moment.locale('ja')
-            translatedPostDate= moment(element.innerHTML).format('LL')
-            break;
+    function translateDate(element, langcode) {
+        switch (langcode) {
+            case 'cn':
+                moment.locale('zh-cn')
+                translatedPostDate = moment(element.innerHTML).format('LL')
+                break
+            case 'kr':
+                moment.locale('ko')
+                translatedPostDate = moment(element.innerHTML).format('LL')
+                break
+            case 'jp':
+                moment.locale('ja')
+                translatedPostDate = moment(element.innerHTML).format('LL')
+                break
+        }
+        element.innerHTML = translatedPostDate
     }
-    element.innerHTML = translatedPostDate;
-}
 
-if(LANGCODE =='jp' || LANGCODE =='kr' || LANGCODE =='cn'){
-    //translate current post date
-    if(currentPostDate) translateDate(document.getElementById("post-date"), LANGCODE) 
-    //translate related posts date
-    if(relatedPostsDates) Array.from(relatedPostsDates).forEach((relatedPostDate) => { translateDate(relatedPostDate, LANGCODE) });
+    if (LANGCODE == 'jp' || LANGCODE == 'kr' || LANGCODE == 'cn') {
+        //translate current post date
+        if (currentPostDate) translateDate(document.getElementById('post-date'), LANGCODE)
+        //translate related posts date
+        if (relatedPostsDates)
+            Array.from(relatedPostsDates).forEach((relatedPostDate) => {
+                translateDate(relatedPostDate, LANGCODE)
+            })
+        if (recentPostsDates)
+            Array.from(recentPostsDates).forEach((recentPostsDate) => {
+                translateDate(recentPostsDate, LANGCODE)
+            })
+    }
 }
